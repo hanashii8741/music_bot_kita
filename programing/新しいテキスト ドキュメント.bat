60ead9b9469df21c3a7fec4aef5ef0598f8ba30c
hanashii8741 <hanasi0817@gmail.com>
2025-05-14 01:58:50 +0900

    Add files via upload

---
+++ programing/新しいテキスト ドキュメント.bat
@@ -0,0 +1,7 @@
+echo "# music_bot_kita" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/hanashii8741/music_bot_kita.git
+git push -u origin main
